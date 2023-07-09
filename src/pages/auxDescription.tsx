b3326b676455c69ae94285620fa1d30518e749ee
arrecode <axelromero99@outlook.com>
2023-07-08 21:41:18 -0300

    fix description

--- src/pages/auxDescription.tsx
+++ src/pages/auxDescription.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Footer from "../components/views/Description";
+import Description from "../views/Description";
 import { Box } from "@chakra-ui/react";
 
 const auxDescription = () => {
   return (
     <>
       <Box bg="blue" w="100" h="100vh"></Box>
-      <Footer />
+      <Description />
       <Box bg="blue" w="100" h="100vh"></Box>
     </>
   );
