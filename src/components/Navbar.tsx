ef4aef7272aba00f2a3ed9a12589d0d30b1b7b38
ARRECODE <axelromero99@outlook.com>
2023-10-06 19:50:21 -0300

    functional navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../styles//Navbar.module.css";
+import { FaHome, FaUser, FaServicestack, FaEnvelope } from "react-icons/fa";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      <>logo</>
-      <>hamburger</>
+    <div className={styles.navbar}>
+      <button onClick={toggleNavbar} className={styles.toggleButton}>
+        {isOpen ? "Close" : "Open"}
+      </button>
+      <nav className={`${styles.nav} ${isOpen ? styles.open : ""}`}>
+        <ul>
+          <li>
+            <a href="#">
+              <FaHome /> Home
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FaUser /> About
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FaServicestack /> Services
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FaEnvelope /> Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import styles from "../styles/Hero.module.css";
+import Navbar from "@/components/Navbar";
 
 const Hero = () => {
   return (
-    <div className={styles.container}>
-      <video
-        className={styles.video}
-        src="/hero-background/2.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-      ></video>
-      <div>
-        <p>
-          we are <span className={styles.not}>!(not)</span>
-        </p>
-        <p>another javascript joke</p>
+    <>
+      <div className={styles.container}>
+        <video
+          className={styles.video}
+          src="/hero-background/2.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+        ></video>
+        <div>
+          <p>
+            we are <span className={styles.not}>!(not)</span>
+          </p>
+          <p>another javascript joke</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
