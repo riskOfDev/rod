53c775d4364ff6f52d3f69306d0547f655762e23
ARRECODE <axelromero99@outlook.com>
2023-10-06 22:15:34 -0300

    modal services

--- src/views/Services.tsx
+++ src/views/Services.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../styles/Services.module.css";
 
 const Services = () => {
-  return <div></div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleExpandClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseClick = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className={styles.services}>
+      <img
+        src="/services/1.png"
+        alt="Service"
+        className={`${styles.image} ${isModalOpen ? styles.hide : ""}`}
+        onClick={handleExpandClick}
+      />
+      {isModalOpen && (
+        <div className={styles.modal} onClick={handleCloseClick}>
+          <img
+            src="/services/1.png"
+            alt="Service"
+            className={styles.modalImage}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Services;
